25ef4b0a83c8b17799b8e30e3ad1f3228a03b9aa
Vokler <valerypavlikov@yandex.ru>
2020-12-14 14:51:00 +0300

    changed name of urls

--- lesson5/geekshop/geekshop/urls.py
+++ lesson5/geekshop/geekshop/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('', mainapp_views.main, name='main'),
     path('products/', include('mainapp.urls', namespace='products')),
     path('auth/', include('authapp.urls', namespace='auth')),
-    path('basket/', include('basketapp.urls', namespace='basket')),
+    path('baskets/', include('basketapp.urls', namespace='baskets')),
 
 ]
 
