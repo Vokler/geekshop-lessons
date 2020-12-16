8023c62826d31ddaa2c274ede96676775358b5ef
Vokler <valerypavlikov@yandex.ru>
2020-12-16 14:41:29 +0300

    added login_required

--- lesson6/geekshop/basketapp/views.py
+++ lesson6/geekshop/basketapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import HttpResponseRedirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.template.loader import render_to_string
 from django.http import JsonResponse
 
@@ -6,6 +7,7 @@ from mainapp.models import Product
 from basketapp.models import Basket
 
 
+@login_required
 def basket_add(request, id=None):
     product = get_object_or_404(Product, id=id)
     baskets = Basket.objects.filter(user=request.user, product=product)
@@ -22,6 +24,7 @@ def basket_add(request, id=None):
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_remove(request, id=None):
     basket = Basket.objects.get(id=id)
     if basket.quantity > 1:
@@ -32,6 +35,7 @@ def basket_remove(request, id=None):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_edit(request, id, quantity):
     if request.is_ajax():
         quantity = int(quantity)
