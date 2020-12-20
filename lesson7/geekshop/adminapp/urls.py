84b0e9ab6702376af09ea9c8204fcaf7ca9dee69
Vokler <valerypavlikov@yandex.ru>
2020-12-20 13:19:29 +0300

    added admin-users-create

--- lesson7/geekshop/adminapp/urls.py
+++ lesson7/geekshop/adminapp/urls.py
@@ -7,4 +7,5 @@ app_name = 'adminapp'
 urlpatterns = [
     path('', adminapp.index, name='index'),
     path('users/', adminapp.admin_users, name='admin_users'),
+    path('users/create/', adminapp.admin_users_create, name='admin_users_create'),
 ]
--- lesson7/geekshop/adminapp/views.py
+++ lesson7/geekshop/adminapp/views.py
@@ -1,13 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 
 from authapp.models import User
+from adminapp.forms import UserAdminRegisterForm
 
 
 def index(request):
     return render(request, 'adminapp/index.html')
 
 
+# Следующие контроллеры демонстрируют принцип CRUD
+
+def admin_users_create(request):
+    # C - Create
+    if request.method == 'POST':
+        form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admin_staff:admin_users'))
+        else:
+            print(form.errors)
+    else:
+        form = UserAdminRegisterForm()
+    context = {'form': form}
+    return render(request, 'adminapp/admin-users-create.html', context)
+
+
 def admin_users(request):
+    # R - Read
     context = {
         'users': User.objects.all(),
     }
