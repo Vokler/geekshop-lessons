a2cb9945955c546feb14a9475231de98b6949e3c
Vokler <valerypavlikov@yandex.ru>
2020-12-20 13:49:03 +0300

    added user_passes_test

--- lesson7/geekshop/adminapp/views.py
+++ lesson7/geekshop/adminapp/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import user_passes_test
 
 from authapp.models import User
 from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def index(request):
     return render(request, 'adminapp/index.html')
 
 
 # Следующие контроллеры демонстрируют принцип CRUD
-
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_create(request):
     # C - Create
     if request.method == 'POST':
@@ -26,6 +28,7 @@ def admin_users_create(request):
     return render(request, 'adminapp/admin-users-create.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users(request):
     # R - Read
     context = {
@@ -34,6 +37,7 @@ def admin_users(request):
     return render(request, 'adminapp/admin-users-read.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_update(request, user_id):
     # U - Update
     user = User.objects.get(id=user_id)
@@ -49,6 +53,7 @@ def admin_users_update(request, user_id):
     return render(request, 'adminapp/admin-users-update-delete.html', context)
 
 
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users_remove(request, user_id):
     user = User.objects.get(id=user_id)
     # user.delete()
