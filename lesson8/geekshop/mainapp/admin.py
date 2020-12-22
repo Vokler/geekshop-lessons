5c892f3b44410df39b2b666c53ed8828b86b485a
Vokler <valerypavlikov@yandex.ru>
2020-12-22 17:57:42 +0300

    fix bug

--- lesson8/geekshop/mainapp/admin.py
+++ lesson8/geekshop/mainapp/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 from mainapp.models import ProductCategory, Product
 
 admin.site.register(ProductCategory)
-admin.site.register(Product
+admin.site.register(Product)
