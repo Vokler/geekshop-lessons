253f5b3c4950db24dbaee6a0d002ca1687afac2c
Vokler <valerypavlikov@yandex.ru>
2020-12-23 12:32:09 +0300

    small changes

--- lesson8/geekshop/mainapp/views.py
+++ lesson8/geekshop/mainapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from mainapp.models import Product, ProductCategory
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def main(request):
@@ -11,17 +12,32 @@ def main(request):
 
 
 def products(request, category_id=None):
-    context = {'title': 'GeekShop - Категории'}
+    """Without pagination."""
+    context = {'title': 'GeekShop - Категории', 'categories': ProductCategory.objects.all()}
     if category_id:
         products = Product.objects.filter(category_id=category_id)
         context.update({
-            'categories': ProductCategory.objects.all(),
             'products': products,
         })
-        return render(request, 'mainapp/products.html', context)
-
-    context.update({
-        'categories': ProductCategory.objects.all(),
-        'products': Product.objects.all(),
-    })
+    else:
+        context.update({
+            'products': Product.objects.all(),
+        })
     return render(request, 'mainapp/products.html', context)
+
+# def products(request, category_id=None):
+#     """With pagination."""
+#     context = {'title': 'GeekShop - Категории'}
+#     if category_id:
+#         products = Product.objects.filter(category_id=category_id)
+#         context.update({
+#             'categories': ProductCategory.objects.all(),
+#             'products': products,
+#         })
+#         return render(request, 'mainapp/products.html', context)
+#
+#     context.update({
+#         'categories': ProductCategory.objects.all(),
+#         'products': Product.objects.all(),
+#     })
+#     return render(request, 'mainapp/products.html', context)
