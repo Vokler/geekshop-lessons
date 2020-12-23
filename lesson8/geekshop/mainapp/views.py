b36ed488cb11f154aaba740d6b7b3cf2724f2b5d
Vokler <valerypavlikov@yandex.ru>
2020-12-23 13:15:47 +0300

    added pagination system

--- lesson8/geekshop/mainapp/views.py
+++ lesson8/geekshop/mainapp/views.py
@@ -11,33 +11,33 @@ def main(request):
     return render(request, 'mainapp/index.html', content)
 
 
-def products(request, category_id=None):
-    """Without pagination."""
-    context = {'title': 'GeekShop - Категории', 'categories': ProductCategory.objects.all()}
-    if category_id:
-        products = Product.objects.filter(category_id=category_id)
-        context.update({
-            'products': products,
-        })
-    else:
-        context.update({
-            'products': Product.objects.all(),
-        })
-    return render(request, 'mainapp/products.html', context)
-
 # def products(request, category_id=None):
-#     """With pagination."""
-#     context = {'title': 'GeekShop - Категории'}
+#     """Without pagination."""
+#     context = {'title': 'GeekShop - Категории', 'categories': ProductCategory.objects.all()}
 #     if category_id:
 #         products = Product.objects.filter(category_id=category_id)
 #         context.update({
-#             'categories': ProductCategory.objects.all(),
 #             'products': products,
 #         })
-#         return render(request, 'mainapp/products.html', context)
-#
-#     context.update({
-#         'categories': ProductCategory.objects.all(),
-#         'products': Product.objects.all(),
-#     })
+#     else:
+#         context.update({
+#             'products': Product.objects.all(),
+#         })
 #     return render(request, 'mainapp/products.html', context)
+
+def products(request, category_id=None, page=1):
+    """Without pagination."""
+    context = {'title': 'GeekShop - Категории', 'categories': ProductCategory.objects.all()}
+    if category_id:
+        products = Product.objects.filter(category_id=category_id).order_by('price')
+    else:
+        products = Product.objects.all().order_by('price')
+    paginator = Paginator(products, 3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+    context.update({'products': products_paginator})
+    return render(request, 'mainapp/products.html', context)
